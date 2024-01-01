0ce01e9a4c3f78e0607c4ddccd3d8a3da5b3fd89
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2024-01-01 08:55:12 -0500

    add new routes - by year, by station_id

--- main.py
+++ main.py
@@ -5,22 +5,42 @@ app = Flask(__name__)
 
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 #  limit data to these 2 columns:
-stations = stations[["STAID","STANAME                                 "]]
+stations = stations[["STAID", "STANAME                                 "]]
+
+
 @app.route("/home")
 def home():
     return render_template("home.html", data=stations.to_html())
 
+
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def station_data_by_date(station, date):
     #  zfill pads the value with 0s to the length specified
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
-    # temperature = df.station(date)
-    # return render_template("about.html")
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def station_data_by_year(station, year):
+    #  zfill pads the value with 0s to the length specified
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["DATE"] = df["    DATE"].astype(str)
+    result = df[df["DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/<station>")
+def all_data_by_station(station):
+    #  zfill pads the value with 0s to the length specified
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
